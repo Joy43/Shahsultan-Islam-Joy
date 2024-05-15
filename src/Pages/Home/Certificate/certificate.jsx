f9e47a3f0fdc259c38f64a70b105ffa753b24bb8
joy43 <ssjoy43@gmail.com>
2024-05-15 18:02:03 +0600

    new certificate elearn db

--- src/Pages/Home/Certificate/certificate.jsx
+++ src/Pages/Home/Certificate/certificate.jsx
@@ -9,6 +9,13 @@ const Certificate = () => {
         "Completed Full stack Web Development Course with Programming Hero",
       image: "https://i.ibb.co/pxkhpct/certificate-page-0001.jpg",
     },
+    {
+      id: "1nRPrVxOIIVWv-bLYg-stR7nLzfkRFK8",
+      name: "Responsive web design course",
+      subtitle:
+        "Completed Responsive web design Video course Elearn DB in bangladesh",
+      image: "https://i.ibb.co/VWTrC36/web-design.jpg",
+    },
   ];
 
   const [open, setOpen] = useState(false);
@@ -25,37 +32,39 @@ const Certificate = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-1">
-      {certificateData.map((certificate, index) => (
-        <div
-          key={index}
-          className="mx-auto max-w-[350px] space-y-4 rounded-lg p-6 shadow-lg md:w-[350px] bg-[#222D56] mb-4"
-        >
-          <img
-            width={200}
-            height={200}
-            className="h-[275px] w-[350px] rounded-lg object-cover"
-            src={
-              certificate.image || "https://source.unsplash.com/200x200/?bed"
-            }
-            alt={certificate.name}
-          />
-          <div className="grid gap-2">
-            <h1 className="text-lg font-semibold">{certificate.name}</h1>
-            <p className="text-sm text-[#94a1cf]">{certificate.subtitle}</p>
+    <div>
+      <div className="flex gap-2 sm:flex-wrap-reverse ">
+        {certificateData.map((certificate, index) => (
+          <div
+            key={index}
+            className="mx-auto max-w-[350px] space-y-4 rounded-lg p-6 shadow-lg md:w-[350px] bg-[#222D56] mb-4"
+          >
+            <img
+              width={200}
+              height={200}
+              className="h-[275px] w-[350px] rounded-lg object-cover"
+              src={
+                certificate.image || "https://source.unsplash.com/200x200/?bed"
+              }
+              alt={certificate.name}
+            />
+            <div className="grid gap-2">
+              <h1 className="text-lg font-semibold">{certificate.name}</h1>
+              <p className="text-sm text-[#94a1cf]">{certificate.subtitle}</p>
+            </div>
+            <div className="text-center">
+              {certificate.id && (
+                <button
+                  onClick={() => handleClickToOpen(certificate)}
+                  className="rounded-lg bg-slate-800 px-6 py-2 text-[12px] font-semibold text-white duration-300 hover:bg-slate-950 sm:text-sm md:text-base"
+                >
+                  Show Now
+                </button>
+              )}
+            </div>
           </div>
-          <div className="text-center">
-            {certificate.id && (
-              <button
-                onClick={() => handleClickToOpen(certificate)}
-                className="rounded-lg bg-slate-800 px-6 py-2 text-[12px] font-semibold text-white duration-300 hover:bg-slate-950 sm:text-sm md:text-base"
-              >
-                Show Now
-              </button>
-            )}
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
       {open && selectedCertificate && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="relative bg-white rounded-lg p-4">
